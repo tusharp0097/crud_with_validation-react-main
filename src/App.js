60f9effcc6a0e6bb5a2d85d0c705b00cdc786d92
tusharp0097 <147375394+tusharp0097@users.noreply.github.com>
2023-10-27 14:18:12 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routing from './Routing/Routing';
+import '../src/styles/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Routing />
+    </>
+  )
+}
+
+export default App
